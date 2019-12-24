3d8a2fd9e5889ee6f9f2a725cb0a51624acd0265
xjinjin <13776572414@163.com>
2019-12-24 09:00:01 +0800

    change

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#nohup python /code/start_file.py >/dev/null 2>&1 &
-/usr/bin/python /code/start_file.py
+nohup python /code/start_file.py >/dev/null 2>&1 &
+#python /code/start_file.py
